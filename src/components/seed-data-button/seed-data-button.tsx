ae9826b88d1067c43a23adf2939cb854b3e837de
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-07-11 15:07:15 +0530

    dev button

--- src/components/seed-data-button/seed-data-button.tsx
+++ src/components/seed-data-button/seed-data-button.tsx
@@ -3,6 +3,10 @@
 import { seed } from '@/db/seed';
 
 export default function SeedDataButton() {
+  if (process.env.NODE_ENV !== 'development') {
+    return null;
+  }
+
   return (
     <button
       onClick={async () => {
