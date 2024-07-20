4f505cf7873afb712664e098731a3f0689e39ea5
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-07-20 23:14:15 +0530

    fix not null

--- src/app/(home)/_actions/index.ts
+++ src/app/(home)/_actions/index.ts
@@ -2,11 +2,13 @@
 
 import { db } from '@/db';
 import { usersQuery } from '@/schema';
+import { isNotNull } from 'drizzle-orm';
 
 export async function getAssignedDepartments() {
   return await db
     .selectDistinct({ value: usersQuery.predAssignment })
     .from(usersQuery)
+    .where(isNotNull(usersQuery.predAssignment))
     .orderBy(usersQuery.predAssignment);
 }
 
@@ -14,5 +16,6 @@ export async function getContactModes() {
   return await db
     .selectDistinct({ value: usersQuery.userQueryMode })
     .from(usersQuery)
+    .where(isNotNull(usersQuery.userQueryMode))
     .orderBy(usersQuery.userQueryMode);
 }
